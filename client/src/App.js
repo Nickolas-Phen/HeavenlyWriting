887b729cdde7164fe13c431db2b84261a32e88e1
Jackwitt77 <jwittmayer@ufl.edu>
2020-03-02 19:34:22 -0500

    created signIn

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 
 const App = () => {
   return (
-    <SignUp/>
+    <SignIn/>
   );
 };
 
