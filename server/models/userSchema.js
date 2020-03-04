8a9f9ba41de932002aa1de258d49f8b03595bad8
melindachhu <mchhu@ufl.edu>
2020-03-03 21:53:58 -0500

    someUserControllers

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -6,13 +6,15 @@ const Schema = new mongoose.Schema;
 
 const userSchema = new Schema ({
     name: {type: String, required: true},
-    birthTime: {type: Number},
-    birthdate: {type: Date, required: true},
+    birthtime: {type: Number},
+    birthday: {type: Date, required: true},
     email: {type: String, required:true},
     house: {type: String},
     zodiac: {type: String},
         
-    username: {type: String, unique:true},
+    username: {type: String, required: true, unique:true},
     password: {type: String}
 
-})
+});
+
+export default mongoose.model('User', userSchema);
