ee88de0034d2c10723b1f65cfec5b1021ffe0e22
Ziyad Morsi <zmorsi@ufl.edu>
2020-03-10 00:04:43 -0400

    Latest updates

--- client/src/components/User/Feedback.js
+++ client/src/components/User/Feedback.js
@@ -2,6 +2,8 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
+import Slider from  "@material-ui/core/Slider";
+import Button from  "@material-ui/core/Button";
 
 const useStyles = makeStyles(theme => ({
   paper: {
@@ -9,8 +11,38 @@ const useStyles = makeStyles(theme => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center"
+  },
+  root: {
+    width: 200,
+  },
+  row: {
+    width: "50%",
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    flexWrap: "wrap"
   }
 }));
+
+const marks = [
+  {
+    value: 0,
+    label: 'Low',
+  },
+  {
+    value: 5,
+    label: 'Moderate',
+  },
+  {
+    value: 10,
+    label: 'High',
+  },
+];
+
+function valuetext(value) {
+  return `${value}`;
+}
 
 export default function Feedback() {
   const classes = useStyles();
@@ -21,7 +53,73 @@ export default function Feedback() {
 
   return (
     <div className={classes.paper}>
-      <Typography paragraph>Tell me where it hurts.</Typography>
+            <Typography paragraph>Tell us your feedback. Rate how you feel:</Typography>
+      <div className={classes.row}>
+        <div className={classes.root}>
+          <Typography id="moodSlider" gutterBottom>Mood</Typography>
+          <Slider
+          defaultValue={5}
+          getAriaValueText={valuetext}
+          aria-labelledby="moodSlider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks={marks}
+          min={0}
+          max={10}
+          onChange={(_event, newValue) => {setMood(newValue);}}
+          />
+        </div>
+        <div className={classes.root}>
+          <Typography id="stressSlider" gutterBottom>Stress</Typography>
+          <Slider
+          defaultValue={5}
+          getAriaValueText={valuetext}
+          aria-labelledby="stressSlider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks={marks}
+          min={0}
+          max={10}
+          onChange={(event, newValue) => {setStress(newValue);}}
+          />
+        </div>
+      </div>
+      <div className={classes.row}>
+        <div className={classes.root}>
+          <Typography id="energySlider" gutterBottom>Energy</Typography>
+          <Slider
+          defaultValue={5}
+          getAriaValueText={valuetext}
+          aria-labelledby="energySlider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks={marks}
+          min={0}
+          max={10}
+          onChange={(event, newValue) => {setEnergy(newValue);}}
+          />
+        </div>
+        <div className={classes.root}>
+          <Typography id="sociabilitySlider" gutterBottom>Sociability</Typography>
+          <Slider
+          defaultValue={5}
+          getAriaValueText={valuetext}
+          aria-labelledby="sociabilitySlider"
+          valueLabelDisplay="auto"
+          step={1}
+          marks={marks}
+          min={0}
+          max={10}
+          onChange={(event, newValue) => {setSociability(newValue);}}
+          />
+        </div>
+      </div>
+
+      <Button 
+      variant="contained"
+      color="primary" 
+      onClick={() => console.log([mood, stress, energy, sociability])}
+      >Submit Feedback</Button>
     </div>
   );
 }
