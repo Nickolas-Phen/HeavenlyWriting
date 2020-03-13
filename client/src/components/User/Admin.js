68e0e9df615483fac970bf739b82a06505ddc354
Jackwitt77 <jwittmayer@ufl.edu>
2020-03-13 14:06:13 -0400

    added dropdowns to admin page

--- client/src/components/User/Admin.js
+++ client/src/components/User/Admin.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import dropdown from "../widgets/dropdown.js"
+import Select from 'react-select'
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+
+const options = [
+  {value: 'chocolate', label: 'Chocolate'},
+  {value: 'strawberry', label: 'Strawberry'},
+  {value: 'vanilla', label: 'Vanilla'}
+];
+
+const DropDown = () => {
+  return <Select options={options}/>
+}
 
 const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
   }
 }));
 
@@ -16,6 +29,19 @@ export default function Admin() {
   return (
     <div className={classes.paper}>
       <Typography paragraph>UNLIMITED POWER.</Typography>
+      <Container component="main" maxWidth= "md">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={3}>
+            <DropDown></DropDown>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <DropDown></DropDown>
+          </Grid>
+          <Grid item xs={12} sm={3}>
+            <DropDown></DropDown>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
