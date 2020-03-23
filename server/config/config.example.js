0af3568ea3be0578d6e4a3b9e817afe8bc5688ca
Kenneth-Sc <KSchultzFL@gmail.com>
2020-03-23 16:05:17 -0400

    Re-added database config

---
+++ server/config/config.example.js
@@ -0,0 +1,14 @@
+export default {
+    db: {
+      uri: 'mongodb+srv://administrator:heavenlywr1t1ng@usersdatabase-f3y79.mongodb.net/test?retryWrites=true&w=majority'
+      // Will need to privatize user and password
+    },
+    databaseOptions: {
+        user     : 'administrator',
+        protected : 'heavenlywr1t1ng',
+        port     : '3000'           // Keeping for now
+    },
+    openCage: {
+      key: 'd8f1bb88bbfa4f7ca0f0484c90a11383' 
+    }, 
+  };
