1a1e7e6936bb74da3fc1fc4fb826a62ce363e134
Kenneth-Sc <school@Kenneths-MacBook-Pro.local>
2020-03-31 01:17:41 -0400

    admin cluster

--- server/models/Schema.js
+++ server/models/Schema.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+/*import mongoose from 'mongoose';
 
 //making a schema for the database
 
@@ -16,3 +16,4 @@ const userSchema = new Schema ({
     password: {type: String}
 
 })
+*/
